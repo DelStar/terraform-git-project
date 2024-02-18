ee76327cc7c43c065f133b4cb6fcc92c3d9c88df
daleyhub <daleystarinc@gmail.com>
2024-02-18 16:45:44 -0500

    updates

--- eks-cluster/nodes.tf
+++ eks-cluster/nodes.tf
@@ -21,7 +21,7 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.large"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 10
     min_size     = 0
   }
